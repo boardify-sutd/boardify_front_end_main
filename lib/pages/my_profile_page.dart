a280638dee546f23f8520aae1f5fa725fdf603ca
GlennChia <glennchia7@gmail.com>
2019-06-22 13:17:36 +0800

    passing values stateful widgets

--- lib/pages/my_profile_page.dart
+++ lib/pages/my_profile_page.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget{
+class ProfilePage extends StatefulWidget{
 
   final String pageText;
 
-  ProfilePage(this.pageText);
+  //const ProfilePage({Key key, this.pageText}): super(key: key);
+  ProfilePage(this.pageText, {Key key}): super(key: key);
+  @override
+  _ProfilePage createState() => _ProfilePage();
+}
+
+
+class _ProfilePage extends State<ProfilePage>{
+
+  //String pageText = _ProfilePage(this.pageText);
 
   @override
   Widget build(BuildContext context){
     return new Scaffold(
-      appBar: new AppBar(title: new Text(pageText),),
+      appBar: new AppBar(title: new Text(widget.pageText),),
       body: new Center(
-        child: new Text(pageText),
+        child: new Text(widget.pageText),
       ),
     );
   }
+
+
 }
